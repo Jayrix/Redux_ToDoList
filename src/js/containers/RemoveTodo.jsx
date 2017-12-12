e5bdb34779da985475d3392f74919e87a2b82825
Jakub Kozlik <kozlik.jakub@gmail.com>
2017-12-12 23:57:21 +0100

    creating remove component button

---
+++ src/js/containers/RemoveTodo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import {removeTodo} from '../actions/actionCreators'
+
+let RemoveTodo = ({dispatch}) => {
+    <button onClick={}>
+        Remove Todo
+    </button>
+};
